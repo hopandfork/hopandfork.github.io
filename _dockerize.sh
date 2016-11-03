0aeeb8a7024e93fda7a55b12d64d9058b2d1ee00
Gabriele Russo Russo <gabri.russo17@gmail.com>
2016-11-03 10:38:06 +0100

    Adds base files.
    We started development of the blog, building on top of Lanyon theme for Jekyll.

---
+++ _dockerize.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if ! docker ps 2>&1 > /dev/null; then
+	sudo systemctl start docker
+fi
+
+docker run --rm --label=jekyll -p 127.0.0.1:4000:4000 --volume=$(pwd):/srv/jekyll jekyll/jekyll:pages jekyll serve --watch
