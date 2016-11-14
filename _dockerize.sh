cdc8d3be4811c39343fb717fda08a1cd1fa57521
Gabriele Russo Russo <gabri.russo17@gmail.com>
2016-11-14 17:11:49 +0100

    Makes _dockerize.sh script interactive
    Now you can send signals to the server using the console.

--- _dockerize.sh
+++ _dockerize.sh
@@ -4,4 +4,4 @@ if ! docker ps 2>&1 > /dev/null; then
 	sudo systemctl start docker
 fi
 
-docker run --rm --label=jekyll -p 127.0.0.1:4000:4000 --volume=$(pwd):/srv/jekyll jekyll/jekyll:pages jekyll serve --watch --drafts
+docker run -it --rm --label=jekyll -p 127.0.0.1:4000:4000 --volume=$(pwd):/srv/jekyll jekyll/jekyll:pages jekyll serve --watch --drafts
